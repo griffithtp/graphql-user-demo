53d41f81ba53918a937045ed745194a181b6f44c
Griffith Tchenpan <griffith.tchenpan@myworkpapers.com>
2017-10-21 23:33:10 +0200

    added CompanyType GraphQLObjectType

--- schema/schema.js
+++ schema/schema.js
@@ -7,12 +7,29 @@ const {
   GraphQLSchema
 } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+});
+
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLInt},
     firstName: { type: GraphQLString},
-    age: { type: GraphQLInt}
+    age: { type: GraphQLInt},
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        //console.log(parentValue, args);
+        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then(res => res.data)
+      }
+    }
   }
 });
 
@@ -25,6 +42,14 @@ const RootQuery = new GraphQLObjectType({
       resolve(parentValue, args) {
         return axios.get(`http://localhost:3000/users/${args.id}`)
           .then(resp => resp.data)
+      }
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLInt}},
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${args.id}`)
+          .then(res => res.data)
       }
     }
   }
