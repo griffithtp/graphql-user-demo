f499e2e8c1cdb2fc45d1e307a876e3fefbea4279
Griffith Tchenpan <griffith.tchenpan@myworkpapers.com>
2017-10-22 00:02:15 +0200

    added fix for resolving circular references

--- schema/schema.js
+++ schema/schema.js
@@ -10,35 +10,34 @@ const {
 
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     description: { type: GraphQLString },
     users: {
       type: new GraphQLList(UserType),
       resolve(parentValue, args) {
-        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}/users`)
+        return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
           .then( res => res.data )
       }
     }
-  }
+  })
 });
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
-    id: { type: GraphQLInt},
+  fields: () => ({
+    id: { type: GraphQLString},
     firstName: { type: GraphQLString},
     age: { type: GraphQLInt},
     company: {
       type: CompanyType,
       resolve(parentValue, args) {
-        //console.log(parentValue, args);
         return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
           .then(res => res.data)
       }
     }
-  }
+  })
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -54,7 +53,7 @@ const RootQuery = new GraphQLObjectType({
     },
     company: {
       type: CompanyType,
-      args: { id: { type: GraphQLInt}},
+      args: { id: { type: GraphQLString}},
       resolve(parentValue, args) {
         return axios.get(`http://localhost:3000/companies/${args.id}`)
           .then(res => res.data)
